4b4f73581c607329bd066561b010b6193acb73fe
meganjeffcoat <jeffcoat.megan@gmail.com>
2019-06-18 16:45:54 -0500

    router set up and imported to server.js

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 
 const server = express();
 
+const postsRouter = require('./router/post-router.js');
+
 server.use(express.json());
+server.use('/api/posts', postsRouter);
 
 server.get('/', (req, res) => {
     res.send(`
